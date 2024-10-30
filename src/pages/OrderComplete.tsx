061a0630d01fe66db3ea566ffa7d2e3bf8c9561a
wuavann5076 <qsao2212@gmail.com>
2024-10-30 21:48:10 +0700

    update order complete

--- src/pages/OrderComplete.tsx
+++ src/pages/OrderComplete.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import backgr from "../assets/bookshelf.jpg";
+import tick from "../assets/tick-circle.png";
 const OrderComplete = () => {
   return (
     <div
       className="w-full REM flex flex-col items-center bg-cover bg-center "
-      style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${backgr})`,
-      }}
+      // style={{
+      //   backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url(${backgr})`,
+      // }}
     >
-      <div className="w-full flex flex-col justify-center items-center py-32 gap-6 min-h-[70vh]">
-        <h1 className="text-white font-bold text-5xl">Đặt hàng thành công!</h1>
-        <h2 className="text-white">
+      <div className="w-full flex flex-col justify-center items-center py-32 gap-6 min-h-[75vh]">
+        <img src={tick} alt="" className="h-40 w-40" />
+        <h1 className="text-black font-bold text-5xl">Đặt hàng thành công!</h1>
+        <h2 className="text-black">
           Bạn có thể kiểm tra lại đơn hàng trong{" "}
           <Link
             to="/accountManagement/purchase"

4f289be1310533115dba442aaf30fd92a7c61b71
coude1 <letrongdaocontact8@gmail.com>
2024-11-04 00:17:08 +0700

    merge

--- src/pages/ProfileUser.tsx
+++ src/pages/ProfileUser.tsx
@@ -1,20 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import Grid from '@mui/material/Grid2';
-import { Button, TextField, Typography, Modal, Box } from '@mui/material';
+import React, { useState } from "react";
+import Grid from "@mui/material/Grid2";
+import { Button, MenuItem, TextField, Typography } from "@mui/material";
 import "../components/ui/ProfileUser.css";
-import { privateAxios } from '../middleware/axiosInstance';
-import { useAppSelector } from '../redux/hooks';
-import { UserInfo, Address } from '../common/base.interface';
+import { privateAxios } from "../middleware/axiosInstance";
+import { useAppSelector } from "../redux/hooks";
+import { UserInfo, Address } from "../common/base.interface";
 // import { LocalizationProvider } from '@mui/x-date-pickers';
 // import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import NewAddressForm from '../components/checkout/NewAddressForm';
+import NewAddressForm from "../components/checkout/NewAddressForm";
 
 interface ProfileData {
-    email: string;
-    name: string;
-    phone: string;
-    address: string;
-    avatar: string;
+  email: string;
+  username: string;
+  phoneNumber: string;
+  gender: string;
+  address: string;
+  dateOfBirth: string;
+  avatar: string;
 }
 
 const modalStyle = {
@@ -32,157 +34,155 @@ const modalStyle = {
 };
 
 const ProfileUser: React.FC = () => {
-    const [editing, setEditing] = useState(false);
-    const [newAvatar, setNewAvatar] = useState<string | null>(null);
-    const [loading, setLoading] = useState(true);
-    const { accessToken } = useAppSelector((state) => state.auth);
-    const [isAddressModalOpen, setIsAddressModalOpen] = useState(false);
-    const [selectedAddress, setSelectedAddress] = useState<Address | null>(null);
-    const [userInfo, setUserInfo] = useState<UserInfo>();
+  const [editing, setEditing] = useState(false);
+  const [newAvatar, setNewAvatar] = useState<string | null>(null);
 
-    const [profileData, setProfileData] = useState<ProfileData>({
-        email: '',
-        name: '',
-        phone: '',
-        address: '',
-        avatar: '',
-    });
+  const [profileData, setProfileData] = useState<ProfileData>({
+    email: "",
+    name: "",
+    phone: "",
+    address: "",
+    avatar: "",
+  });
 
-    const fetchUserAddress = async () => {
-        try {
-            const response = await privateAxios("/user-addresses/user");
+  const fetchUserAddress = async () => {
+    try {
+      const response = await privateAxios("/user-addresses/user");
 
-            const data = response.data;
-            console.log("address", data);
+      const data = response.data;
+      console.log("address", data);
 
+      //   const sortedAddresses = data.sort((a: Address, b: Address) => {
+      //     return (b.isDefault ? 1 : 0) - (a.isDefault ? 1 : 0);
+      //   });
+      //   console.log(sortedAddresses);
 
-            //   const sortedAddresses = data.sort((a: Address, b: Address) => {
-            //     return (b.isDefault ? 1 : 0) - (a.isDefault ? 1 : 0);
-            //   });
-            //   console.log(sortedAddresses);
+      //   setSelectedAddress(sortedAddresses[0] || null);
+      //   setAddresses(sortedAddresses);
+    } catch {
+      console.log("...");
+    }
+  };
 
-            //   setSelectedAddress(sortedAddresses[0] || null);
-            //   setAddresses(sortedAddresses);
-        } catch {
-            console.log("...");
-        }
-    };
+  const fetchUserInfo = async () => {
+    if (accessToken) {
+      try {
+        const response = await privateAxios.get("users/profile");
+        const { email, name, phone, address, avatar } = response.data;
+        setUserInfo(response.data);
+        setProfileData({
+          email,
+          name,
+          phone,
+          address,
+          avatar,
+        });
+      } catch (error) {
+        console.error("Error fetching user info:", error);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      setLoading(false);
+    }
+  };
 
-    const fetchUserInfo = async () => {
-        if (accessToken) {
-            try {
-                const response = await privateAxios.get("users/profile");
-                const { email, name, phone, address, avatar } = response.data;
-                setUserInfo(response.data);
-                setProfileData({
-                    email,
-                    name,
-                    phone,
-                    address,
-                    avatar,
-                });
-            } catch (error) {
-                console.error("Error fetching user info:", error);
-            } finally {
-                setLoading(false);
-            }
-        } else {
-            setLoading(false);
-        }
-    };
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) setNewAvatar(URL.createObjectURL(file));
+  };
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const file = event.target.files?.[0];
-        if (file) setNewAvatar(URL.createObjectURL(file));
-    };
+  const handleEditClick = () => setEditing(true);
 
-    const handleEditClick = () => setEditing(true);
+  const handleCancelClick = () => {
+    setEditing(false);
+    setNewAvatar(null);
+  };
 
-    const handleCancelClick = () => {
-        setEditing(false);
-        setNewAvatar(null);
-    };
+  const handleConfirmClick = () => {
+    if (newAvatar) setProfileData({ ...profileData, avatar: newAvatar });
+    setEditing(false);
+  };
 
-    const handleConfirmClick = () => {
-        if (newAvatar) setProfileData({ ...profileData, avatar: newAvatar });
-        setEditing(false);
-    };
+  const handleAddressModalClose = () => {
+    setIsAddressModalOpen(false);
+    fetchUserInfo(); // Refresh user info after address update
+  };
 
-    const handleAddressModalClose = () => {
-        setIsAddressModalOpen(false);
-        fetchUserInfo(); // Refresh user info after address update
-    };
+  const refreshAddresses = () => {
+    fetchUserInfo();
+  };
 
-    const refreshAddresses = () => {
-        fetchUserInfo();
-    };
+  useEffect(() => {
+    fetchUserInfo();
+    fetchUserAddress();
+  }, [accessToken]);
 
-    useEffect(() => {
-        fetchUserInfo();
-        fetchUserAddress();
-    }, [accessToken]);
+  return (
+    // <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <div className="profile-container w-full">
+      <Typography
+        sx={{
+          fontSize: "35px",
+          fontWeight: "bolder",
+          textAlign: "center",
+          marginBottom: "20px",
+        }}
+        className="profile-title"
+      >
+        HỒ SƠ CỦA TÔI
+      </Typography>
 
-    return (
-        // <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <div className="profile-container w-full">
-            <Typography
-                sx={{ fontSize: '35px', fontWeight: 'bolder', textAlign: 'center', marginBottom: '20px' }}
-                className="profile-title"
-            >
-                HỒ SƠ CỦA TÔI
-            </Typography>
+      <Grid container spacing={0} alignItems="center">
+        <Grid size={3}>
+          <div className="profile-image">
+            <img
+              src={newAvatar || profileData.avatar}
+              alt="avatar"
+              className="avatar-image1"
+              style={{ width: "150px", height: "auto", borderRadius: "50%" }}
+            />
+            {editing && (
+              <>
+                <label htmlFor="avatar-upload" className="change-photo-label">
+                  <Typography color="primary" className="change-photo-text">
+                    Đổi ảnh đại diện
+                  </Typography>
+                </label>
+                <input
+                  id="avatar-upload"
+                  type="file"
+                  accept="image/*"
+                  onChange={handleFileChange}
+                  style={{ display: "none" }}
+                />
+              </>
+            )}
+          </div>
+        </Grid>
 
-            <Grid container spacing={0} alignItems="center">
-                <Grid size={3} sx={{ display: 'flex', justifyContent: 'flex-end', pr: 0 }}>
-                    <div className="profile-image">
-                        <img
-                            src={newAvatar || profileData.avatar}
-                            alt="avatar"
-                            className="avatar-image1"
-                            style={{ width: '150px', height: 'auto', borderRadius: '50%' }}
-                        />
-                        {editing && (
-                            <>
-                                <label htmlFor="avatar-upload" className="change-photo-label">
-                                    <Typography color="primary" className="change-photo-text">
-                                        Đổi ảnh đại diện
-                                    </Typography>
-                                </label>
-                                <input
-                                    id="avatar-upload"
-                                    type="file"
-                                    accept="image/*"
-                                    onChange={handleFileChange}
-                                    style={{ display: 'none' }}
-                                />
-                            </>
-                        )}
-                    </div>
-                </Grid>
-
-                <Grid size={9}>
-                    <form noValidate autoComplete="off" className="profile-form">
-                        {[
-                            { label: 'Email', name: 'email', value: profileData.email, disabled: true },
-                            { label: 'Username', name: 'name', value: profileData.name },
-                            { label: 'Số Điện Thoại', name: 'phone', value: profileData.phone },
-                        ].map(({ label, name, value, disabled }) => (
-                            <div key={name} className="form-row">
-                                <Typography>{label}:</Typography>
-                                <TextField
-                                    fullWidth
-                                    margin="normal"
-                                    variant="outlined"
-                                    value={value}
-                                    onChange={(e) => setProfileData({ ...profileData, [name]: e.target.value })}
-                                    disabled={disabled}
-                                    className="profile-field"
-                                    size="small"
-                                />
-                            </div>
-                        ))}
-
-                        {/* <div className="form-row">
+        <Grid size={9}>
+          <form noValidate autoComplete="off" className="profile-form">
+            {[
+              { label: "Email", name: "email", disabled: true },
+              { label: "Username", name: "name" },
+              { label: "Số Điện Thoại", name: "phone" },
+            ].map(({ label, name, disabled }) => (
+              <div key={name} className="form-row">
+                <Typography>{label}:</Typography>
+                <TextField
+                  fullWidth
+                  margin="normal"
+                  variant="outlined"
+                  name={name}
+                  value={(profileData as any)[name]}
+                  disabled={disabled}
+                  className="profile-field"
+                  size="small"
+                />
+              </div>
+            ))}
+            {/* <div className="form-row">
                             <Typography>Địa Chỉ:</Typography>
                             <div className="flex items-center gap-2">
                                 <TextField
@@ -209,59 +209,59 @@ const ProfileUser: React.FC = () => {
                                 </Button>
                             </div>
                         </div> */}
-                    </form>
-                </Grid>
-            </Grid>
+          </form>
+        </Grid>
+      </Grid>
 
-            <div className="button-container">
-                {!editing ? (
-                    <Button
-                        variant="contained"
-                        onClick={handleEditClick}
-                        sx={{ fontSize: '20px', backgroundColor: '#000', color: '#fff' }}
-                    >
-                        Cập Nhật Hồ Sơ
-                    </Button>
-                ) : (
-                    <>
-                        <Button
-                            variant="contained"
-                            color="error"
-                            onClick={handleCancelClick}
-                            sx={{ fontSize: '20px', marginRight: '10px' }}
-                        >
-                            Hủy
-                        </Button>
-                        <Button
-                            variant="contained"
-                            color="success"
-                            onClick={handleConfirmClick}
-                            sx={{ fontSize: '20px' }}
-                        >
-                            Xác Nhận
-                        </Button>
-                    </>
-                )}
-            </div>
-
-            <Modal
-                open={isAddressModalOpen}
-                onClose={handleAddressModalClose}
-                aria-labelledby="address-modal-title"
+      <div className="button-container">
+        {!editing ? (
+          <Button
+            variant="contained"
+            onClick={handleEditClick}
+            sx={{ fontSize: "20px", backgroundColor: "#000", color: "#fff" }}
+          >
+            Cập Nhật Hồ Sơ
+          </Button>
+        ) : (
+          <>
+            <Button
+              variant="contained"
+              color="error"
+              onClick={handleCancelClick}
+              sx={{ fontSize: "20px", marginRight: "10px" }}
             >
-                <Box sx={modalStyle}>
-                    {userInfo && (
-                        <NewAddressForm
-                            userInfo={userInfo}
-                            onClose={handleAddressModalClose}
-                            refreshAddresses={refreshAddresses}
-                        />
-                    )}
-                </Box>
-            </Modal>
-        </div>
-        // </LocalizationProvider>
-    );
+              Hủy
+            </Button>
+            <Button
+              variant="contained"
+              color="success"
+              onClick={handleConfirmClick}
+              sx={{ fontSize: "20px" }}
+            >
+              Xác Nhận
+            </Button>
+          </>
+        )}
+      </div>
+
+      <Modal
+        open={isAddressModalOpen}
+        onClose={handleAddressModalClose}
+        aria-labelledby="address-modal-title"
+      >
+        <Box sx={modalStyle}>
+          {userInfo && (
+            <NewAddressForm
+              userInfo={userInfo}
+              onClose={handleAddressModalClose}
+              refreshAddresses={refreshAddresses}
+            />
+          )}
+        </Box>
+      </Modal>
+    </div>
+    // </LocalizationProvider>
+  );
 };
 
 export default ProfileUser;

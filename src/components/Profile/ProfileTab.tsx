d0402f346bd3666d0a07fe1a8d95deacccb137ae
wuavann5076 <qsao2212@gmail.com>
2024-11-08 21:31:03 +0700

    update route for profile

--- src/components/Profile/ProfileTab.tsx
+++ src/components/Profile/ProfileTab.tsx
@@ -1,29 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
-const ProfileTab: React.FC = () => {
+interface ProfileTabProp {
+  currentUrl: string;
+}
+
+const ProfileTab: React.FC<ProfileTabProp> = ({ currentUrl }) => {
   const [activeTab, setActiveTab] = useState(0);
-
+  const navigate = useNavigate();
   const tabs = [
-    "HOẠT ĐỘNG GẦN ĐÂY",
-    "TRUYỆN ĐANG BÁN",
-    "BÀI ĐĂNG TRAO ĐỔI",
-    "TRUYỆN ĐỂ TRAO ĐỔI",
-    "LỊCH SỬ TRAO ĐỔI",
+    { label: "HOẠT ĐỘNG GẦN ĐÂY", path: "/profile/recentActivities" },
+    { label: "TRUYỆN ĐANG BÁN", path: "/profile/comicSelling" },
+    { label: "BÀI ĐĂNG TRAO ĐỔI", path: "/profile/postExchange" },
+    { label: "TRUYỆN ĐỂ TRAO ĐỔI", path: "/profile/comicExchange" },
+    { label: "LỊCH SỬ TRAO ĐỔI", path: "/profile/historyExchange" },
   ];
+
+  useEffect(() => {
+    const currentIndex = tabs.findIndex((tab) => tab.path === currentUrl);
+    if (currentIndex !== -1) {
+      setActiveTab(currentIndex);
+    }
+  }, [currentUrl, tabs]);
 
   return (
     <div className="flex">
       {tabs.map((tab, index) => (
         <button
           key={index}
-          onClick={() => setActiveTab(index)}
+          onClick={() => {
+            setActiveTab(index);
+            navigate(tab.path);
+          }}
           className={`flex-1 px-4 py-2 text-center duration-200 hover:opacity-70 text-nowrap text-sm ${
             activeTab === index
               ? "border-b-2 border-black"
               : "bg-white text-black"
           }`}
         >
-          {tab}
+          {tab.label}
         </button>
       ))}
     </div>

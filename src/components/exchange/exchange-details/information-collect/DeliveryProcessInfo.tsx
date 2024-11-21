812bc7e99e91484163ebb905b72222672fdec07d
wuavann5076 <qsao2212@gmail.com>
2024-11-22 03:25:11 +0700

    update delivery UI

--- src/components/exchange/exchange-details/information-collect/DeliveryProcessInfo.tsx
+++ src/components/exchange/exchange-details/information-collect/DeliveryProcessInfo.tsx
@@ -1,18 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Car from "../../../../assets/car.png";
 import { LinearProgress } from "@mui/material";
-const DeliveryProcessInfo: React.FC = () => {
-  const deliveryStatus = "Đang giao hàng"; // Thay đổi trạng thái tại đây
-  const estimatedDeliveryTime = "20:00 - 21:00";
-  const sender = {
-    name: "Nguyễn Văn A",
-    address: "123 Đường ABC, Quận 1, TP. Hồ Chí Minh",
-  };
-  const receiver = {
-    name: "Lê Thị B",
-    address: "456 Đường XYZ, Quận 2, TP. Hồ Chí Minh",
+import moment from "moment/min/moment-with-locales";
+import { ExchangeDetails } from "../../../../common/interfaces/exchange.interface";
+import { privateAxios } from "../../../../middleware/axiosInstance";
+import { Delivery } from "../../../../common/interfaces/delivery.interface";
+moment.locale("vi");
+export default function DeliveryProcessInfo({
+  firstUserName,
+  exchangeDetails,
+  secondUserName,
+  firstAddress,
+  secondAddress,
+}: {
+  exchangeDetails: ExchangeDetails;
+  firstUserName: string;
+  secondUserName: string;
+  firstAddress: string;
+  secondAddress: string;
+}) {
+  const [deliveryDetails, setDeliveryDetails] = useState<{
+    fee: number;
+    estTime: Date;
+  }>();
+  const [total, setTotal] = useState(0);
+
+  const exchange = exchangeDetails.exchange;
+
+  const fetchDeliveryFeeAndDeliveryTime = async () => {
+    await privateAxios
+      .get(`deliveries/exchange/from-user/${exchange.id}`)
+      .then(async (res) => {
+        const userDelivery: Delivery = res.data;
+        await privateAxios
+          .get(`deliveries/details/${userDelivery.id}`)
+          .then((res) => {
+            setDeliveryDetails({
+              fee: res.data.deliveryFee,
+              estTime: res.data.estDeliveryTime,
+            });
+
+            setTotal(
+              exchange.depositAmount! +
+                res.data.deliveryFee +
+                (exchangeDetails.isRequestUser
+                  ? 0
+                  : exchange.compensationAmount)
+            );
+          })
+          .catch((err) => console.log(err));
+      });
   };
 
+  useEffect(() => {
+    fetchDeliveryFeeAndDeliveryTime();
+  }, [exchangeDetails]);
+  const deliveryStatus = "Đang giao hàng"; // Thay đổi trạng thái tại đây
+  const [status, setStatus] = useState<string>("DELIVERED");
+  const formatDate =
+    moment(deliveryDetails?.estTime)
+      .format("dddd DD/MM")
+      .charAt(0)
+      .toUpperCase() +
+    moment(deliveryDetails?.estTime).format("dddd DD/MM/yyyy").slice(1);
   return (
     <div className="p-6 bg-white rounded-lg shadow-lg REM overflow-hidden">
       <h2 className="text-lg font-bold text-gray-700 mb-4">
@@ -22,14 +72,14 @@ const DeliveryProcessInfo: React.FC = () => {
         <div className="w-1/2 flex flex-col">
           <div className="mb-4">
             <h3 className="font-semibold text-gray-800">Người gửi:</h3>
-            <p className="font-light">{sender.name}</p>
-            <p className="font-light">{sender.address}</p>
+            <p className="font-light">{firstUserName}</p>
+            <p className="font-light">{firstAddress}</p>
           </div>
 
           <div className="mb-4">
             <h3 className="font-semibold text-gray-800">Người nhận:</h3>
-            <p className="font-light">{receiver.name}</p>
-            <p className="font-light">{receiver.address}</p>
+            <p className="font-light">{secondUserName}</p>
+            <p className="font-light">{secondAddress}</p>
           </div>
         </div>
         <div className="w-1/2 flex flex-col gap-4">
@@ -42,19 +92,22 @@ const DeliveryProcessInfo: React.FC = () => {
 
           <div className="mb-4">
             <h3 className="font-semibold text-gray-800">Thời gian dự kiến:</h3>
-            <p className="font-light">{estimatedDeliveryTime}</p>
+            <p className="font-light">{formatDate}</p>
           </div>
         </div>
       </div>
-
-      <div className="mt-5 r">
-        <p className="w-full text-center text-sm font-light italic pb-4">
-          Trên đường giao hàng đến bạn...
-        </p>
-        <LinearProgress />
-      </div>
+      {status === "DELIVERED" ? (
+        <button className="w-full bg-sky-700 text-white font-semibold py-2 rounded-md hover:opacity-70 duration-200">
+          Đã giao hàng
+        </button>
+      ) : (
+        <div className="mt-5 r">
+          <p className="w-full text-center text-sm font-light italic pb-4">
+            Trên đường giao hàng đến bạn...
+          </p>
+          <LinearProgress />
+        </div>
+      )}
     </div>
   );
-};
-
-export default DeliveryProcessInfo;
+}
--- src/pages/ExchangeDetail.tsx
+++ src/pages/ExchangeDetail.tsx
@@ -119,7 +119,7 @@ const ExchangeDetail: React.FC = () => {
               (delivery.to && delivery.to.user.id === first?.id)
           );
           if (firstUserDelivery) {
-            setFirstCurrentStage(3);
+            setFirstCurrentStage(4);
             setFirstAddress(
               deliveries[0].from.user.id === first?.id
                 ? deliveries[0].from.name
@@ -133,7 +133,7 @@ const ExchangeDetail: React.FC = () => {
               (delivery.to && delivery.to.user.id === second?.id)
           );
           if (secondUserDelivery) {
-            setSecondCurrentStage(3);
+            setSecondCurrentStage(4);
             setSecondAddress(
               deliveries[0].from.user.id === second?.id
                 ? deliveries[0].from.name
